10ca980ec074ac831ae8f95d2c9431f4e0d500b7
jeovazero <contato@jeova.ninja>
2021-06-28 00:28:40 -0300

    feat(app): decode tweets

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,14 @@ module Main where
 
 import System.Environment
 import Janitor
+import Data.Tweets
 
 main = do
     token <- lookupEnv "TOKEN"
     twitterID <- lookupEnv "TWITTER_ID"
 
     case (token, twitterID) of
-        (Just tk, Just id) -> readTweets tk id
+        (Just tk, Just id) -> do
+            result <- readTweets tk id
+            putStrLn $ show $ decodeTweets result
         _ -> putStrLn "Missing TOKEN or TWITTER_ID env var"
