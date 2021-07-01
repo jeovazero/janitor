d788d68b3baa05fd7e456ff40608500101316187
jeovazero <contato@jeova.ninja>
2021-07-01 01:01:29 -0300

    feat(oauth1): add percent-encoding

--- src/OAuth1.hs
+++ src/OAuth1.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 import System.Random
 import Data.List as L
 import Data.Word
@@ -5,6 +6,7 @@ import Data.Char
 import Data.ByteString as B
 import Data.ByteString.Base64
 import Data.Time.Clock.POSIX
+import Data.Map.Strict as Map
 
 rndRange = randomR (0,255)
 gen = rndRange . snd
@@ -22,3 +24,46 @@ nonce = do
 
 -- in millis
 timestamp = fmap (truncate . (* 1000)) getPOSIXTime
+
+percentEncodingMap :: Map.Map Word8 ByteString
+percentEncodingMap =
+    Map.fromList $ fmap (\(a,b) -> (fromIntegral $ ord a, b))
+        [('!', "%21")
+        ,('#', "%23")
+        ,('$', "%24")
+        ,('%', "%25")
+        ,('&', "%26")
+        ,('\'', "%27")
+        ,('(', "%28")
+        ,(')', "%29")
+        ,('*', "%2A")
+        ,('+', "%2B")
+        ,(',', "%2C")
+        ,('/', "%2F")
+        ,(':', "%3A")
+        ,(';', "%3B")
+        ,('=', "%3D")
+        ,('?', "%3F")
+        ,('@', "%40")
+        ,('[', "%5B")
+        ,(']', "%5D")
+        ]
+
+percentEncoding bs
+  | B.null bs = bs
+  | otherwise = B.concat $ percentEncoding' bs 1 (B.length bs) []
+
+percentEncoding' bs current end chuncks =
+    case (isEnd, Map.lookup (B.index bs (current - 1)) percentEncodingMap) of
+        (True, Just value) -> L.reverse (bs:addChunck value)
+        (True, _) -> L.reverse (bs:chuncks)
+        (False, Just value) ->
+            percentEncoding' bs'' 1 end' (addChunck value)
+        _ -> percentEncoding' bs (current + 1) end chuncks
+    
+    where
+        isEnd = current == end
+        end' = end - current
+        (as,bs') = B.splitAt (current - 1) bs
+        bs'' = B.drop 1 bs'
+        addChunck c = c:as:chuncks
