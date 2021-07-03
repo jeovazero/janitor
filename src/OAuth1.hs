dbddede992a2a6b1b8f312d12c62858022f5dcc4
jeovazero <contato@jeova.ninja>
2021-07-02 23:57:28 -0300

    feat(oauth1): add the generator for oauth1 header

--- src/OAuth1.hs
+++ src/OAuth1.hs
@@ -1,12 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+module OAuth1 (oauth1Header,OAuth1HeaderParams(..)) where
 import System.Random
 import Data.List as L
 import Data.Word
 import Data.Char
 import Data.ByteString as B
+import Data.ByteString.Char8 as B8
 import Data.ByteString.Base64
 import Data.Time.Clock.POSIX
 import Data.Map.Strict as Map
+import Crypto.Hash.SHA1 (hmac)
+
+data OAuth1HeaderParams = OAuth1HeaderParams {
+        parameters :: [(ByteString,ByteString)],
+        method :: ByteString,
+        url :: ByteString,
+        oauthToken :: ByteString,
+        oauthSecret :: ByteString,
+        consumerKey :: ByteString,
+        consumerSecret :: ByteString
+    }
 
 rndRange = randomR (0,255)
 gen = rndRange . snd
@@ -20,10 +34,10 @@ randomBytes size = do
 
 nonce = do
     rb <- randomBytes 32
-    pure $ B.filter (isAlphaNum . chr . fromIntegral) $ encode (pack rb)
+    pure $ B.filter (isAlphaNum . chr . fromIntegral) $ encode (B.pack rb)
 
 -- in millis
-timestamp = fmap (truncate . (* 1000)) getPOSIXTime
+timestamp = fmap truncate getPOSIXTime
 
 percentEncodingMap :: Map.Map Word8 ByteString
 percentEncodingMap =
@@ -68,12 +82,41 @@ percentEncoding' bs current end chuncks =
         bs'' = B.drop 1 bs'
         addChunck c = c:as:chuncks
 
-parameterString =
-    B.intercalate "&"
-        . L.sort
-            . fmap (\(a,b) -> B.concat [percentEncoding a,"=", percentEncoding b])
+parameterString separator =
+    B.intercalate separator
+    . L.sort
+    . fmap (\(a,b) -> B.concat [percentEncoding a,"=", percentEncoding b])
 
 toUpperW8 = fromIntegral . ord . toUpper . chr . fromIntegral
+numberToBytestring = B8.pack . show
 
 signatureBaseString method url parameters =
-    B.intercalate "&" [B.map toUpperW8 method,percentEncoding url,parameterString parameters]
+    B.intercalate "&" [B.map toUpperW8 method,percentEncoding url,percentEncoding $ parameterString "&" parameters]
+
+oauth1Header (OAuth1HeaderParams {
+    oauthToken,
+    oauthSecret,
+    consumerKey,
+    consumerSecret,
+    url,
+    method,
+    parameters 
+}) = do
+    n <- nonce
+    t <- timestamp
+    let oauthParameters =
+            [("oauth_nonce",n)
+            ,("oauth_timestamp", numberToBytestring t)
+            ,("oauth_consumer_key",consumerKey)
+            ,("oauth_signature_method","HMAC-SHA1")
+            ,("oauth_token",oauthToken)
+            ,("oauth_version","1.0")
+            ]
+    let parameters' = oauthParameters ++ parameters
+    let signatureBase = signatureBaseString method url parameters'
+    let signingKey = B.intercalate "&" $ fmap percentEncoding [consumerSecret,oauthSecret]
+    let oauthSignature = encode $ hmac signingKey signatureBase
+    let allParameters = ("oauth_signature",oauthSignature):parameters'
+    print (signatureBase)
+
+    pure $ B.concat ["OAuth ",parameterString ", " allParameters]
