e8778e236fea30dc65e3faf7e9f4b47b11975110
jeovazero <contato@jeova.ninja>
2021-07-03 23:53:34 -0300

    feat(janitor): add verify-credentials

--- src/Janitor.hs
+++ src/Janitor.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Janitor (readTweets) where
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+module Janitor (readTweets,verifyCredentials) where
 
 import qualified Data.ByteString.Lazy.Char8 as LB
 import qualified Data.ByteString.Char8 as B
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import OAuth1 as O
 
 baseV2 = "https://api.twitter.com/2"
 
@@ -17,6 +20,38 @@ readTweets token userID = do
         requestHeaders = [
             ("User-Agent", "haskell"),
             ("Authorization", B.concat ["Bearer ",B.pack token])
+        ]
+    }
+    response <- httpLbs request manager
+
+    pure $ responseBody response
+
+
+baseV1 = "https://api.twitter.com/1.1"
+
+verifyCredentials :: String -> String -> String -> String  -> IO LB.ByteString 
+verifyCredentials oauthToken' oauthSecret' consumerKey' consumerSecret' = do
+    manager <- newManager tlsManagerSettings
+
+    let url' = concat [baseV1,"/account/verify_credentials.json"]
+    initialRequest <- parseRequest url'
+    
+    let params = OAuth1HeaderParams {
+        parameters = [],
+        method = "GET",
+        url = B.pack url',
+        oauthToken = B.pack oauthToken',
+        oauthSecret = B.pack oauthSecret',
+        consumerKey = B.pack consumerKey',
+        consumerSecret = B.pack consumerSecret'
+    }
+    authHeader <- oauth1Header params
+    print authHeader
+
+    let request = initialRequest {
+        requestHeaders = [
+            ("User-Agent", "haskell"),
+            ("Authorization", authHeader)
         ]
     }
     response <- httpLbs request manager
